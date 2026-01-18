3c2edfe58b76ff194b691e9fc73e53cb36ab8440
Luca <darkartek@gmail.com>
2026-01-18 04:52:51 +0100

    fix(ci): grant write permissions to release workflow

--- cri-crm-core.php
+++ cri-crm-core.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: CRI CRM Core
  * Description: Core CRM functionalities for CRI Venezia (Chat, Admin, Fundraising) - Replaces external Cloudflare Worker.
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: CRI Venezia
  * Text Domain: cri-crm
  */
